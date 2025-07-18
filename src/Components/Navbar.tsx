72c78ee3cd0313de729580b33b3b2f7d9af256fb
nAB5N <mongus2064@gmail.com>
2025-07-18 17:02:33 +0545

    Navbar Finished

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,34 +1,66 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { Book } from "lucide-react";
+import { AlignLeft, Book } from "lucide-react";
 import Link from "next/link";
 import Maxwidthwrapper from "./Maxwidthwrapper";
 
 export default function Navbar() {
-  return (
-    <div>
-        <Maxwidthwrapper>
-      <nav className="flex justify-between">
-        <div className="flex items-center">
-          <div>
-            <Book className="text-blue-800 font-bold " size={48} />
-          </div>
-          <div>
-            <p className="font-bold text-3xl">BookHaven</p>
-          </div>
-        </div>
+  const [isopen, setIsOpen] = useState(false);
 
-        <div className="flex gap-2">
+  return (
+    <div className=" bg-slate-50 shadow-2xl">
+      <Maxwidthwrapper>
+        <nav className="flex justify-between p-4 backdrop-blur-3xl">
+          <div className="flex items-center gap-0.5">
             <div>
-                <Link href={"/login"}>SignIn</Link>
+              <Book className="text-blue-800 font-bold lg:w-14  lg:h-14 w-6 h-6 " />
             </div>
             <div>
-                <Link href={"/signup"}>SignUp</Link>
+              <p className="font-bold text-xl lg:text-3xl">BookHaven</p>
             </div>
-        </div>
-      </nav>
+          </div>
+          <div onClick={() => setIsOpen(!isopen)} className=" lg:hidden  flex items-center">
+            <AlignLeft />
+          </div>
+          <div className=" hidden lg:flex gap-4 items-center">
+              <div className="hover:text-blue-800 hover:scale-105 duration-250 ">
+                <Link className="font-semibold" href={"/login"}>
+                  Sign In
+                </Link>
+              </div>
+
+              <div className="hover:scale-105 transition-transform hover:text-blue-800 duration-300 ">
+                <Link
+                  className="bg-white p-2 rounded-md border font-semibold"
+                  href={"/signup"}
+                >
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+        </nav>
+         {isopen ? (
+            <div className="flex gap-4 items-center">
+              <div className="hover:text-blue-800 hover:scale-105 duration-250 ">
+                <Link className="font-semibold" href={"/login"}>
+                  Sign In
+                </Link>
+              </div>
+
+              <div className="hover:scale-105 transition-transform hover:text-blue-800 duration-300 ">
+                <Link
+                  className="bg-white p-2 rounded-md border font-semibold"
+                  href={"/signup"}
+                >
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
       </Maxwidthwrapper>
     </div>
   );
