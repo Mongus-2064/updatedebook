fd8fc45508be6ddc81d087fb4e4099b164be2024
nAB5N <mongus2064@gmail.com>
2025-07-21 15:47:35 +0545

    Implemented loading

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -17,51 +17,55 @@ export default function Navbar() {
               <Book className="text-blue-800 font-bold lg:w-14  lg:h-14 w-6 h-6 " />
             </div>
             <div>
-              <p className="font-bold text-xl lg:text-3xl">BookHaven</p>
+              <Link href={"/"} className="font-bold text-xl lg:text-3xl">
+                BookHaven
+              </Link>
             </div>
           </div>
-          <div onClick={() => setIsOpen(!isopen)} className=" lg:hidden  flex items-center">
+          <div
+            onClick={() => setIsOpen(!isopen)}
+            className=" lg:hidden  flex items-center"
+          >
             <AlignLeft />
           </div>
           <div className=" hidden lg:flex gap-4 items-center">
-              <div className="hover:text-blue-800 hover:scale-105 duration-250 ">
-                <Link className="font-semibold" href={"/login"}>
-                  Sign In
-                </Link>
-              </div>
-
-              <div className="hover:scale-105 transition-transform hover:text-blue-800 duration-300 ">
-                <Link
-                  className="bg-white p-2 rounded-md border font-semibold"
-                  href={"/signup"}
-                >
-                  Sign Up
-                </Link>
-              </div>
+            <div className="hover:text-blue-800 hover:scale-105 duration-250 ">
+              <Link className="font-semibold" href={"/login"}>
+                Sign In
+              </Link>
             </div>
+
+            <div className="hover:scale-105 transition-transform hover:text-blue-800 duration-300 ">
+              <Link
+                className="bg-white p-2 rounded-md border font-semibold"
+                href={"/signup"}
+              >
+                Sign Up
+              </Link>
+            </div>
+          </div>
         </nav>
-         {isopen ? (
-            <div className="flex gap-4 items-center">
-              <div className="hover:text-blue-800 hover:scale-105 duration-250 ">
-                <Link className="font-semibold" href={"/login"}>
-                  Sign In
-                </Link>
-              </div>
-
-              <div className="hover:scale-105 transition-transform hover:text-blue-800 duration-300 ">
-                <Link
-                  className="bg-white p-2 rounded-md border font-semibold"
-                  href={"/signup"}
-                >
-                  Sign Up
-                </Link>
-              </div>
+        {isopen ? (
+          <div className="flex gap-4 items-center">
+            <div className="hover:text-blue-800 hover:scale-105 duration-250 ">
+              <Link className="font-semibold" href={"/login"}>
+                Sign In
+              </Link>
             </div>
-          ) : (
-            ""
-          )}
+
+            <div className="hover:scale-105 transition-transform hover:text-blue-800 duration-300 ">
+              <Link
+                className="bg-white p-2 rounded-md border font-semibold"
+                href={"/signup"}
+              >
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </Maxwidthwrapper>
-      
     </div>
   );
 }
--- src/app/(home)/components/Bookcard.tsx
+++ src/app/(home)/components/Bookcard.tsx
@@ -1,14 +1,20 @@
+"use client"
+
 import Maxwidthwrapper from "@/Components/Maxwidthwrapper";
 import { Books } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Bookcard({ books }: { books: Books }) {
+
+const [loading, setLoading] = useState<boolean>(false);
+
   return (
     <div className="mt-4">
       <Maxwidthwrapper>
-        <div className="rounded-md p-4 bg-slate-50 shadow-2xl border border-gray-200 hover:scale-105 transition-transform">
+        <div className="rounded-md p-4 bg-slate-50 shadow-2xl border border-gray-200 hover:scale-105 transition-transform duration-300 hover:cursor-pointer
+        ">
             <div className="flex  justify-center">
           <Image
             src={books.imageLink}
@@ -20,12 +26,15 @@ export default function Bookcard({ books }: { books: Books }) {
         </div>
         <hr className="text-gray-200 mt-4"/>
         <div className="flex flex-col gap-3">
-          <p className="text-blue-500 font-semibold">{books.title}</p>
+          <p className="text-blue-500 font-semibold text-balance truncate">{books.title}</p>
           <p className="text-gray-600">by {books.author}</p>
           <p className="text-gray-600">{books.description}</p>
           <Link
+          onClick={()=> setLoading(true)}
           className="bg-blue-500 text-center px-6 py-3 text-white rounded-md hover:scale-105 transition-transform"
-          href={"/books"}>View Details</Link>
+          href={`/book/${books.id}`}> {
+            loading ? <p>Loading...</p> : <p>View Details</p>
+          }</Link>
         </div>
         </div>
         
