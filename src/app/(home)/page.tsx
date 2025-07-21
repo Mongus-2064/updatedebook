c9c634dfd3e1fb09ec54981d13e1cada7287f145
nAB5N <mongus2064@gmail.com>
2025-07-21 08:11:25 +0545

    Dummy data added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,18 @@
 import Herosection from '@/app/(home)/components/Herosection'
 import React from 'react'
 import Trust from './components/Trust'
+import Booklist from './components/Booklist'
+import { Books } from '@/utils/dummydata'
 
-export default function Home() {
+
+export default  function Home() {
+
+  console.log(Books)
   return (
     <div>
       <Herosection/>
       <Trust/>
+      <Booklist/>
     </div>
   )
 }
